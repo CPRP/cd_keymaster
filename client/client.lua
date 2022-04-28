8c6b519aff3d6ee2521bdacc3efe0ede3f80a561
Jordan <jordanbscott12@gmail.com>
2022-04-28 17:48:12 -0400

    Adjusted DefaultSettings

--- client/client.lua
+++ client/client.lua
@@ -4,14 +4,14 @@ local NUI_status = false
 local DefaultSettings = {
     settings = {
         handleEnd = true;  --Send a result message if true and callback when message closed or callback immediately without showing the message
-        speed = 10; --pixels / second
+        speed = 8; --pixels / second
         scoreWin = 1000; --Score to win
-        scoreLose = -150; --Lose if this score is reached
+        speed = 8; --pixels / second
         maxTime = 60000; --sec
-        maxMistake = 5; --How many missed keys can there be before losing
-        speedIncrement = 1; --How much should the speed increase when a key hit was successful
+        maxMistake = 8; --How many missed keys can there be before losing
+        speedIncrement = .2; --How much should the speed increase when a key hit was successful
     },
-    keys = {"a", "w", "d", "s", "g"}; --You can hash this out if you want to use default keys in the java side.
+    keys = {"a", "w", "d", "s"}; --You can hash this out if you want to use default keys in the java side.
 }
 
 function StartKeyMaster(settings)
